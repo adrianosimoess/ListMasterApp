ef35111ebc07dcb4757c4d5c8d8de4c00b02040b
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-13 21:42:13 -0400

    commit lista

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListMasterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> lista_de_valor = new List<string>();
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PageMinhaLista(lista_de_valor));
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            lista_de_valor.Add("Meu Item Adicionando");
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
